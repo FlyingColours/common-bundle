e4a2b5c9103b4828030f0a93fbb9a69c9a8e94f7
Alex van Andel <me@alexvanandel.com>
2017-09-05 19:46:30 +0100

    Extracted logic to separate function, fixed test
    Tests were failing because explode() behaviour returns an array with an empty string when it parses an empty string. This results in a header that ends with "," - although browsers can deal very well with this formatting error, I thought it wise to not rely on browsers fixing formatting mistakes.

--- src/Listener/CorsHeadersListener.php
+++ src/Listener/CorsHeadersListener.php
@@ -13,22 +13,12 @@ class CorsHeadersListener
     {
         $request = $event->getRequest();
         $response = $event->getResponse();
-
-        $accessControlExposeHeaders = [ 'CSRF-TOKEN' ];
-
-        if ($response->getStatusCode() === 201) {
-            // expose location in case of 201 response
-            $accessControlExposeHeaders[] = 'Location';
-        }
         
-        $additionalAccessControlExposeHeaders = explode(',', $response->headers->get('Access-Control-Expose-Headers', ''));
-
-        array_push($accessControlExposeHeaders, ...$additionalAccessControlExposeHeaders);
-        array_unique($accessControlExposeHeaders);
+        $accessControlExposeHeaders = $this->getAccessControlExposeHeaders($response);
 
         $response->headers->set('Access-Control-Allow-Origin', $request->headers->get('Origin'));
         $response->headers->set('Access-Control-Allow-Headers', $request->headers->get('Access-Control-Request-Headers'));
-        $response->headers->set('Access-Control-Expose-Headers', join(',', $accessControlExposeHeaders));
+        $response->headers->set('Access-Control-Expose-Headers', join(',', $this->getAccessControl));
         $response->headers->set('Access-Control-Allow-Methods', $request->headers->get('Access-Control-Request-Method'));
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
     }
@@ -39,5 +29,27 @@ class CorsHeadersListener
         {
             $event->setResponse(new Response());
         }
+    }
+    
+    private function getAccessControlExposeHeaders(Response $response): array
+    {
+        $accessControlExposeHeaders = [ 'CSRF-TOKEN' ];
+
+        if ($response->getStatusCode() === 201) {
+            // expose location in case of 201 response
+            $accessControlExposeHeaders[] = 'Location';
+        }
+
+        $additionalAccessControlExposeHeaders = array_filter(
+            explode(',', $response->headers->get('Access-Control-Expose-Headers', ''))
+        );
+
+        if ( ! empty($additionalAccessControlExposeHeaders) ) {
+            array_push($accessControlExposeHeaders, ...$additionalAccessControlExposeHeaders);
+        }
+
+        array_unique($accessControlExposeHeaders);
+
+        return $accessControlExposeHeaders;
     }
 }
